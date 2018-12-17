e754f462e7b98f2555730210a805c29061536cdb
thomp <thompdump@gmail.com>
2018-12-17 05:16:20 -0800

    Adds sanity check for scan_region; improves barcodeScan docstring

--- pdfxcb/barScan.py
+++ pdfxcb/barScan.py
@@ -76,10 +76,26 @@ from PIL import Image
 # imagePNGPath should be a string defining the location of a PNG file
 def barcodeScan(imagePNGPath, scan_region):
     """
-    Return None if a barcode was not found. If a barcode was found, return a string corresponding to the barcode-encoded data. Search within the region defined by SCAN_REGION when SCAN_REGION is a list. SCAN_REGION specifies two points as [x1,y1,x2,y2]. These two points (x1,y1) and (x2,y2) are pairs (x,y) of percentages (expressed as value between 0 and 1.0) relative to the dimensions of the image; they define the box within which the barcode scan occurs
+    Return None if a barcode was not found. If a barcode was found,
+    return a string corresponding to the barcode-encoded data. Search
+    within the region defined by SCAN_REGION when SCAN_REGION is a
+    list. If SCAN_REGION is not a list, the full image is analyzed.
+    When SCAN_REGION is a list, it specifies two points as
+    [x1,y1,x2,y2]. These two points (x1,y1) and (x2,y2) are pairs
+    (x,y) of percentages (each expressed as a value between 0 and 1.0)
+    relative to the dimensions of the image; they define the box
+    within which the barcode scan occurs.
     """
+    # sanity check(s)
     if not isinstance(scan_region,list):
         scan_region = None
+    else:
+        for value in scan_region:
+            if (value < 0 or value > 1):
+                msg = json1.json_msg(999,"insane scan region value",False,None)
+                lg.error(msg)
+                lg.info(json1.json_last_log_msg())
+                sys.exit(msg)
     # obtain image data either via PIL or CV2/numpy
     #   1. using pil
     # PIL origin (0,0) is top left corner
